e4090ab1bb41b8ba1bc18aec7bb002396219ea12
Shohruh <shokh.nosirov@gmail.com>
2018-11-18 14:28:46 -0500

    books-details-p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './books-list/books-list.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksListComponent
+    BooksListComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule,
